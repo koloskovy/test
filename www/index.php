ff0a82f73f5f5827b3c42f4b021fbcb883062afc
koloskovy <koloskovy@mail.ru>
2017-08-09 19:06:27 +0300

    Добавил html код

--- www/index.php
+++ www/index.php
@@ -6,3 +6,41 @@ require __DIR__ . '/functions/file.php';
 
 
  ?>
+
+
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Views</title>
+</head>
+<body>
+<!-- Перенесем эту форму в form.php
+    <form action="add.php" method="post" enctype="multipart/form-data">
+	<label for="title">Название</label>
+	<input type="text" id="title" name="title" >
+	<br>
+	<label for="image">Файл</label>
+	<input type="file" id="image" name="image" >
+	<br>
+	<input type="submit">
+
+
+
+
+</form> -->
+
+<!--А здесь подключаем файл form.php -->
+<?php 
+
+include __DIR__ . '/form.php';
+
+ ?>
+
+
+<a href="./">На главную</a>
+	
+
+	
+</body>
+</html>
